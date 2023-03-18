f4d9a1796b2a69aa855920fba1d5a85af8cbce99
Kunal G <kunalgursal2923@gmail.com>
2023-03-18 22:55:10 +0530

    4th commit

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native-web";
 const home = () => {
   return (
     <View>
-      <Text>Hello World first react native app</Text>
+      <Text>Hello World </Text>
     </View>
   );
 };
