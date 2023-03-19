5a4eaabe9f49d1049f62405ba31bf20680301916
Kunal G <kunalgursal2923@gmail.com>
2023-03-19 23:07:51 +0530

    popular jobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import { View, Text, TouchableOpacity, FlatList } from 'react-native'
-
-import styles from './popularjobs.style'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useRouter } from 'next/router'
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import styles from './popularjobs.style';
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs </Text>
+        <TouchableOpacity>
+          <Text>Show all</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-export default Popularjobs
+export default Popularjobs;
